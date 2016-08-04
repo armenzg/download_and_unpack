b9efb2fe2619ba22f90e2f58fde7d05eaa24812a
Armen Zambrano G <armenzg@mozilla.com>
2016-08-04 14:00:32 -0400

    We can now create zip files

--- create_archives.sh
+++ create_archives.sh
@@ -8,14 +8,19 @@
 # fi
 
 # Using a log is a much smaller file to download
-filename=live_backing.log
-if [ ! -e "$filename" ]; then
-  wget -q -N https://public-artifacts.taskcluster.net/X_Z6IquRSoKiQMPctHK0PA/0/public/logs/live_backing.log
-fi
+# filename=live_backing.log
+# if [ ! -e "$filename" ]; then
+#  wget -q -N https://public-artifacts.taskcluster.net/X_Z6IquRSoKiQMPctHK0PA/0/public/logs/live_backing.log
+# fi
+
+hello=hello_world.txt
+echo "Hello World!" > $hello
+bye=bye_world.txt
+echo "Sayonara!" > $bye
 
 
 if [ ! -e archive.tar ]; then
-  tar -cf archive.tar "$filename"
+  tar -cf archive.tar "$hello" "$bye"
 fi
 
 if [ ! -e archive.tar.gz ]; then
@@ -25,3 +30,9 @@ fi
 if [ ! -e archive.tar.bz2 ]; then
   bzip2 -fk archive.tar >archive.tar.bz2
 fi
+
+if [ ! -e archive.zip ]; then
+  zip archive.zip "$hello" "$bye"
+fi
+
+rm "$hello" "$bye"
