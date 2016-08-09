3af9ccc62e43d423fed16e194f7d462a02271b2f
Armen Zambrano G <armenzg@mozilla.com>
2016-08-09 11:54:30 -0400

    Clean up code

--- create_archives.sh
+++ create_archives.sh
@@ -2,25 +2,13 @@
 # Script to auto-generate the different archive types under the archives directory.
 # Based on whimboo's code
 
-# filename=Firefox\ 47.0.1.dmg
-# if [ ! -e "$filename" ]; then
-#   wget -q -N https://download-installer.cdn.mozilla.net/pub/firefox/releases/47.0.1/mac/en-US/Firefox%2047.0.1.dmg
-# fi
-
-# Using a log is a much smaller file to download
-# filename=live_backing.log
-# if [ ! -e "$filename" ]; then
-#  wget -q -N https://public-artifacts.taskcluster.net/X_Z6IquRSoKiQMPctHK0PA/0/public/logs/live_backing.log
-# fi
-
-hello=hello_world.txt
-echo "Hello World!" > $hello
-bye=bye_world.txt
-echo "Sayonara!" > $bye
-
+filename=live_backing.log
+if [ ! -e "$filename" ]; then
+  wget -q -N https://public-artifacts.taskcluster.net/X_Z6IquRSoKiQMPctHK0PA/0/public/logs/live_backing.log
+fi
 
 if [ ! -e archive.tar ]; then
-  tar -cf archive.tar "$hello" "$bye"
+  tar -cf archive.tar "$filename"
 fi
 
 if [ ! -e archive.tar.gz ]; then
@@ -32,7 +20,5 @@ if [ ! -e archive.tar.bz2 ]; then
 fi
 
 if [ ! -e archive.zip ]; then
-  zip archive.zip "$hello" "$bye"
+  zip archive.zip "$filename"
 fi
-
-rm "$hello" "$bye"
--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ set -ex
 ./create_archives.sh
 echo "Here are the averages with the new unpacking methods with local files:"
 python download.py --times 100
-echo "Here are the averages with the new unpacking methods with real files:"
+echo "Here are the averages with the new unpacking methods with production files:"
 python download.py --url http://archive.mozilla.org/pub/firefox/tinderbox-builds/mozilla-central-linux64/1470319163/firefox-51.0a1.en-US.linux-x86_64.common.tests.zip
 python download.py --url http://archive.mozilla.org/pub/firefox/tinderbox-builds/mozilla-central-linux64/1470319163/firefox-51.0a1.en-US.linux-x86_64.tar.bz2
+# XXX: Until we fix where we extract these files to
+rm -rf bin/ certs/ config/ exte* firefox* jetpack/ jit-test/ jsreftest/ luciddream/ mach marionette/ modules/ mozbase/ mozinfo.json puppeteer/ steeplechase/ tools/ tps/
