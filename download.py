7ca1aac8d5a4fd64ea0fe9f715ac4ee8d00efb4b
Armen Zambrano G <armenzg@mozilla.com>
2016-08-09 11:47:16 -0400

    Move common parser to the main script and delete module

--- download.py
+++ download.py
@@ -6,11 +6,19 @@ import urllib2
 import zipfile
 import zlib
 
+from argparse import ArgumentParser
 from cStringIO import StringIO
 
-from common import parse_args
-
 cwd = os.path.dirname(os.path.realpath(__file__))
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--url", dest="url",
+                        help='File to download and unpack.')
+    parser.add_argument("--times", dest="times", default=1, type=int,
+                        help='How many times to test download a file.')
+    return parser.parse_args()
 
 
 # Not being used at the moment
